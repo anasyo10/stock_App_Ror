fa7abd93692741c8b65ec63a5cdffde92a489c91
anasyo10 <anasyo10@gmail.com>
2017-11-30 15:40:49 +0500

    added fname and lname to registration process

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,5 +9,13 @@ class UserStocksController < ApplicationController
         flash[:success] = "Stock #{@user_stock.stock.name} added to your portfolio"
         redirect_to my_portfolio_path 
     end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        @user_stock.delete
+        flash[:success] = "The stock was successfully deleted"
+        redirect_to my_portfolio_path
+    end
 end
 
